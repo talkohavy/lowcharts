3cd40048f0cc61e3c2f427a80c8f2a67a4cc699e
Tal Kohavy <talkohavy@gmail.com>
2023-07-08 19:17:28 +0300

    dashboards: some behind the scenes work

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: `${process.cwd()}`,
   server: { port: 3000, strictPort: true, open: true },
   plugins: [react()],
 });
