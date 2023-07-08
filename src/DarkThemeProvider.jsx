2742e0036b4a220ab5768fa61db06fe67ed441fb
Tal Kohavy <talkohavy@gmail.com>
2023-07-08 22:37:12 +0300

    dashboards: refactored DarkThemeProvider a little

--- src/DarkThemeProvider.jsx
+++ src/DarkThemeProvider.jsx
@@ -1,19 +1,34 @@
 import React, { createContext, useState } from 'react';
 
+const THEME_OPTIONS = { dark: 'dark', light: 'light' };
+
 export const GetDarkThemeContext = createContext(null);
 export const ToggleDarkThemeContext = createContext(null);
 
 export default function DarkThemeProvider({ children }) {
   const [isDarkMode, setIsDarkMode] = useState(() => {
-    const themeModeInLocalStorage = localStorage.getItem('theme');
-    return themeModeInLocalStorage
-      ? themeModeInLocalStorage === 'dark'
-      : window.matchMedia('(prefers-color-scheme: dark)').matches;
+    // Step 1: get theme from localStorage
+    const localStorageTheme = localStorage.getItem('theme');
+
+    // Step 2: get theme of device (holds a fallback of "light" within)
+    const deviceTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? THEME_OPTIONS.dark
+      : THEME_OPTIONS.light;
+
+    // Step 3: current theme is... (localStorage takes precedence over device)
+    const currentTheme = localStorageTheme || deviceTheme;
+
+    // Step 4: set html's theme based on currentTheme
+    document.body.setAttribute('class', currentTheme);
+
+    // Step 5: return isDarkTheme boolean
+    return currentTheme === THEME_OPTIONS.dark;
   });
 
   const toggleDarkMode = () => {
-    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
-    document.body.setAttribute('class', isDarkMode ? 'light' : 'dark');
+    const themeToBe = isDarkMode ? THEME_OPTIONS.light : THEME_OPTIONS.dark;
+    localStorage.setItem('theme', themeToBe);
+    document.body.setAttribute('class', themeToBe);
     setIsDarkMode(!isDarkMode);
   };
 
